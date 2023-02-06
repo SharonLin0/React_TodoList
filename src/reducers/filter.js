e36237ab0d4a2da8734b6a95daa9b470932e5d42
sharon <sharon.lin@buyandship.com>
2023-02-07 03:13:39 +0800

    [modi]use hash router instead of browser router, add filter intial state

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,7 +1,12 @@
 import { ACTION_TYPES } from '../actions/common'
 import { FilterState } from '../common/utils'
 
-export default function filter(state = FilterState.ALL, action) {
+const initialFilterState = () => {
+  const path = window.location.hash
+  return path.replace(/#(\/)/g, '').toUpperCase() || FilterState.ALL
+}
+
+export default function filter(state = initialFilterState(), action) {
   switch (action.type) {
     case ACTION_TYPES.SET_FILTER:
       return action.filter
