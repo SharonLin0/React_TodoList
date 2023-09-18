9da2a7f6a9030b0ee38f03f7634697bbe5271e26
Hsuan <starfly02727@gmail.com>
2023-09-18 20:28:41 +0800

    modi: add grafana faro monitor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { initializeFaro } from '@grafana/faro-web-sdk';
 import store from './store'
 import App from './components/App'
 import './index.css'
@@ -16,3 +17,15 @@ root.render(
     </Provider>
   </React.StrictMode>
 )
+
+initializeFaro({
+  // Mandatory, the URL of the Grafana Cloud collector with embedded application key.
+  // Copy from the configuration page of your application in Grafana.
+  url: 'http://faro-collector-us-central-0.grafana.net/collect/{app-key}',
+
+  // Mandatory, the identification label(s) of your application
+  app: {
+    name: 'todolist',
+    version: '1.0.0', // Optional, but recommended
+  },
+});
